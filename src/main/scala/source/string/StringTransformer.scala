71528d3a686d4da47be74fb23418d9e1a2487e35
Job Tiel Groenestege <job.tiel@newcraftgroup.com>
2018-12-19 16:10:35 +0100

    Use a trait for the transformer
    - classes start with uppercase
    - put the regexps in as constants (that's a lot faster)
    - Add TODO for replacing SimpleDateFormat

--- src/main/scala/source/string/StringTransformer.scala
+++ src/main/scala/source/string/StringTransformer.scala
@@ -1,14 +1,21 @@
 package source.string
 
-class StringTransformer {
+trait StringTransformer {
 
-  implicit class matchDataType(s: String) {
-    def matchDataType: Any = {
-      val numberFormat = raw"[0-9]+".r
-      val floatFormat = raw"[0-9]+\\.[0-9]+".r
-      val dateMonthFormat = raw"[0-9]{4}-[0-9]{2}".r
-      val dateFullFormat = raw"[0-9]{4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2}+".r
+  object MatchDataType {
 
+    final val numberFormat = raw"[0-9]+".r
+    final val floatFormat = raw"[0-9]+\\.[0-9]+".r
+    final val dateMonthFormat = raw"[0-9]{4}-[0-9]{2}".r
+    final val dateFullFormat = raw"[0-9]{4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2}+".r
+
+  }
+
+  implicit class MatchDataType(s: String) {
+
+    import MatchDataType._
+
+    def matchDataType: Any =
       s match {
         case numberFormat() => s.toString.toInt
         case floatFormat() => s.toString.toFloat
@@ -16,11 +23,7 @@ class StringTransformer {
         case dateFullFormat() => new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(s)
         case _ => s.trim()
       }
-    }
   }
 
 }
 
-object StringTransformer {
-  val matcher = new StringTransformer
-}
